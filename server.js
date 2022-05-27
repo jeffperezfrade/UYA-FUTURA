bdcf0d3f3f2ec551b648080bce8e1c23f9e7bacd
jeffperezfrade <alu0101038520@ull.edu.es>
2022-05-27 15:44:10 +0100

    changes to heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/futura-uya'));
+app.use(express.static('./dist/futura'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/futura-uya/'}),
+    res.sendFile('index.html', {root: 'dist/futura/'}),
 );
 
 // Start the app by listening on the default Heroku port
